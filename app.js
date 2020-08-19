ceb4d17f5a044649d217519649091ae9d01c5ab1
sibykarthikeyan <siby.karthii@gmail.com>
2020-08-19 13:58:31 +0530

    mysql connection

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
+const mysql = require('mysql');
 
 app.use(helmet());
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  
 app.get("/getBillAmount",(req,res)=>{
     res.status(200);
    res.send("Second build travis"); 
